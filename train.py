51234f0e6f5c5187d4fa6fd795e4d2cf2242bb2d
srikanth13578 <srikanthsriko@gmail.com>
2026-06-04 15:39:50 +0530

    update

--- train.py
+++ train.py
@@ -3,3 +3,23 @@
 # Dep : {910,1200,1120,1130,1900,2000}
 # Max Platform Required ?
 
+n = 6
+arr = [900,940,1100,1500,1800]
+dep = [910,1200,1120,1130,1900,2000]
+
+i = 0
+j = 0
+platform = 0
+maxPlatform = 0
+
+while i < n and j < n:
+    if arr[i] < dep[j]:
+        platform+= 1
+        if platform > maxPlatform:
+            maxPlatform = platform
+        i+= 1
+    else:
+        platform = 1
+        j+= 1
+
+print(f" Max Platform : {maxPlatform} ")
